2e5981c864d24dd2bb4342ed43f45e1f64ca009c
Meulk <menushilaksh5@gmail.com>
2020-08-03 07:23:43 +0530

    initial commit

---
+++ src/library/assistant/data/callback/GenericCallback.java
@@ -0,0 +1,10 @@
+package library.assistant.data.callback;
+
+/**
+ *
+ * @author Menushi Lakshika
+ */
+public interface GenericCallback {
+
+    Object taskCompleted(Object val);
+}
---
+++ src/library/assistant/ui/callback/BookReturnCallback.java
@@ -0,0 +1,7 @@
+package library.assistant.ui.callback;
+
+
+
+public interface BookReturnCallback {
+    public void loadBookReturn(String bookID);
+}
